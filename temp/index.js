c501e09ef7dbcb0e3143d14a6b09782107e24f13
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-02-24 15:17:39 +0700

    xoa tem

--- temp/index.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import { TapGestureHandler } from 'react-native-gesture-handler';
-
-AppRegistry.registerComponent(appName, () => App);
